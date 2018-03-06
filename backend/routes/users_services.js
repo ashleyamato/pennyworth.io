5a720cbd7972628a92f8cf6a397d8618e361f6e8
Ashley Amato <ashleyamato1@gmail.com>
2018-03-06 13:47:09 -0700

    users_services path route

--- backend/routes/users_services.js
+++ backend/routes/users_services.js
@@ -3,30 +3,8 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
-//
-// router.get('/', (req, res, next)=> {
-//   return knex('services')
-//   .then(services => {
-//     let promises = services.map(service => {
-//       return knex('users')
-//         .select('users.id', 'users.first_name', 'users.last_name')
-//         .join('users_services', 'users.id', 'users_services.user_id')
-//         .where('users_services.user_id', service.id)
-//         .then(users => {
-//           service.users = users
-//           return service
-//         })
-//         .catch((err) => {
-//           next(err)
-//         })
-//     })
-//     Promise.all(promises).then(results => {
-//       res.status(200).json(results)
-//     })
-//   })
-// })
 
-router.get('/', (req, res, next)=> {
+router.get('/', (req, res, next) => {
   return knex('users')
   .then(users => {
     let promises = users.map(user => {
@@ -34,6 +12,7 @@ router.get('/', (req, res, next)=> {
         .select('services.id', 'services.label')
         .join('users_services', 'services.id', 'users_services.service_id')
         .where('users_services.user_id', user.id)
+        .select('users_services.notes', 'users_services.date', 'users_services.time')
         .then(services => {
           user.services = services
           return user
@@ -48,6 +27,17 @@ router.get('/', (req, res, next)=> {
   })
 })
 
-
+router.patch('/:id', (req, res, next) => {
+  let id =req.params.id
+  let body = req.body
+  console.log(req.body)
+  return knex('users_services')
+  .where('id', id)
+  .update('notes', body.notes)
+  .returning('*')
+  .then((results) => {
+    res.status(200).send(results[0])
+  })
+})
 
 module.exports = router
--- backend/test/route.test.js
+++ backend/test/route.test.js
@@ -23,18 +23,18 @@ suite('CRUD routes', () => {
       }], done);
   });
 
-  // test('POST /services should create a new service and return the name and message that were created.', (done) => {
-  //   request(server)
-  //     .post('/services')
-  //     .set('Accept', 'application/json')
-  //     .send({
-  //       label:'test:clean'
-  //     })
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, {
-  //       label:'test:clean'
-  //     }, done);
-  //   });
+  test('POST /services should create a new service and return the name and message that were created.', (done) => {
+    request(server)
+      .post('/services')
+      .set('Accept', 'application/json')
+      .send({
+        label:'test:clean'
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        label:'test:clean'
+      }, done);
+    });
 
     test('GET /pennyworths/:id should respond with an error if an id is not found.', (done) => {
       request(server)
@@ -44,5 +44,101 @@ suite('CRUD routes', () => {
         .expect(404, { error: "not found" }, done);
     });
 
+    test('GET /users_services should return user and associated services.', (done) => {
+      request(server)
+        .get('/users_services')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(function(res){
+          for (var i = 0; i < res.body.length; i++) {
+            delete res.body[i].created_at
+            delete res.body[i].updated_at
+          }
+        })
+        .expect(200, [{
+            "id": 1,
+            "first_name": "Ashley",
+            "last_name": "Amato",
+            "email": "ashleyamato1@gmail.com",
+            "token": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+            "pennyworth_id": 1,
+            "address": "1111 Bear Mountain Dr, Boulder, CO, 80303",
+            "services": [
+              {
+                "id": 3,
+                "label": "Packages & Mail",
+                "notes": "Package from Amazon should have arrived, please put on table",
+                "date": "2018-03-27",
+                "time": "10:00AM"
+              },
+              {
+                "id": 1,
+                "label": "Tidy-up",
+                "notes": "Please pickup the clothes in the bathroom",
+                "date": "2018-03-27",
+                "time": "10:00AM"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "first_name": "Olivia",
+            "last_name": "Burgener",
+            "email": "livburgener@gmail.com",
+            "token": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+            "pennyworth_id": 2,
+            "address": "2222 Chiron Street , Lafayette, CO, 80026",
+            "services": [
+              {
+                "id": 4,
+                "label": "Grocery Shopping",
+                "notes": "Eggs, Milk, Whole Wheat Bread, Bananas",
+                "date": "2018-04-04",
+                "time": "09:00AM"
+              }
+            ]
+          },
+          {
+            "id": 3,
+            "first_name": "Eddie",
+            "last_name": "Marovich",
+            "email": "eddiemarovich@gmail.com",
+            "token": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+            "pennyworth_id": 2,
+            "address": "1111 Lovin It Lane, Boulder, CO, 80303",
+            "services": [
+              {
+                "id": 5,
+                "label": "Pickup Prescription",
+                "notes": "Please pickup prescription at Walgreens on US-287",
+                "date": "2018-04-04",
+                "time": "09:00AM"
+              }
+            ]
+          }], done)
+      })
 
-});
+    test('PATCH /messages/:id should update a message and return the id, name, and message that were updated.', (done) => {
+      request(server)
+        .patch('/users_services/1')
+        .set('Accept', 'application/json')
+        .send({
+          notes:'Please pickup the clothes in the bathroom'
+        })
+        .expect('Content-Type', /json/)
+        .expect(function(res){
+          delete res.body.created_at
+          delete res.body.updated_at
+        })
+        .expect(200, {
+          id:1,
+          user_id: 1,
+          service_id: 1,
+          notes:'Please pickup the clothes in the bathroom',
+          date: '2018-03-27',
+          time: '10:00AM',
+        }, done)
+
+    })
+
+})
