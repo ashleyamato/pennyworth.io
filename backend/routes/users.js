c91c5a7b3fe62ea32d5eafb338b3b9b1d952eee8
Olivia <livburgener@gmail.com>
2018-03-13 15:47:50 -0600

    almost working get, gets user info need to add join

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,15 +3,15 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
-const admin = require('firebase-admin')
-
-const dbURL = 'http://localhost:3001' // or database url
-const serviceAccount = './config/pennyworth-cd634770f026.json' // get this file from firebase
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: dbURL
-})
+// const admin = require('firebase-admin')
+//
+// const dbURL = 'http://localhost:3001' // or database url
+// const serviceAccount = './config/pennyworth-cd634770f026.json' // get this file from firebase
+//
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: dbURL
+// })
 
 router.get('/', (req, res, next) => {
    return knex('users')
@@ -24,16 +24,25 @@ router.get('/', (req, res, next) => {
   })
 })
 
-
-router.get('/:id', (req, res, next) => {
-  let token = req.params.id
-
-  admin.auth().verifyIdToken(`${token}`)
-    .then(decodedToken => {
-      console.log('you are logged in >>>', decodedToken)
-    })
-    .catch(error => console.log('error', error))
-})
+// 
+// router.get('/:id', (req, res, next) => {
+//   let token = req.params.id
+//   console.log('token', token)
+//   admin.auth().verifyIdToken(`${token}`)
+//     .then(decodedToken => {
+//       let uid = decodedToken.uid
+//       console.log(uid);
+//       knex('users')
+//       .select('id', 'first_name', 'last_name',
+//     'email', 'address', 'pennyworker_id')
+//       .where('token', uid)
+//       .then(data => {
+//         console.log(data)
+//         res.send(data[0])
+//       })
+//     })
+//     .catch(error => console.log('error', error))
+// })
 
 
 router.post('/', (req, res, next) => {
