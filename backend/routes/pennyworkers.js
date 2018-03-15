6dad00a2e0dca67616f69a269b6d16d767a135a7
Ashley Amato <ashleyamato1@gmail.com>
2018-03-15 16:29:35 -0600

    post requestfor user_services complete

--- backend/routes/pennyworkers.js
+++ backend/routes/pennyworkers.js
@@ -15,21 +15,34 @@ router.get('/', (req, res, next) => {
   })
 })
 
+// router.get('/:id', (req,res,next) => {
+//   let id = req.params.id
+//   return knex('pennyworkers')
+//   .where('id', id)
+//   .select('id', 'first_name', 'last_name')
+//   .then(data => {
+//     if(data.length != 0){
+//       res.send(data[0])
+//     } else {
+//       res.status(404).send({ error: "not found" })
+//     }
+//   })
+//   .catch(err => {
+//     res.status(404).send(err)
+//   })
+// })
+
+
 router.get('/:id', (req,res,next) => {
   let id = req.params.id
-  return knex('pennyworkers')
-  .where('id', id)
-  .select('id', 'first_name', 'last_name')
+  knex('users')
+  .join('pennyworkers', 'pennyworkers.id', 'users.pennyworker_id')
+  .select('pennyworkers.id', 'pennyworkers.first_name', 'pennyworkers.last_name')
+  .where('users.id', id)
   .then(data => {
-    if(data.length != 0){
-      res.send(data[0])
-    } else {
-      res.status(404).send({ error: "not found" })
-    }
-  })
-  .catch(err => {
-    res.status(404).send(err)
+    res.status(200).send(data[0])
+  }).catch(err => {
+    next(err)
   })
 })
-
 module.exports = router
